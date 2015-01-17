df5dd0fbf4415cd64d4c0bc5474500667778fda4
Chris Kuehl <ckuehl@ocf.berkeley.edu>
2015-01-16 21:32:32 -0800

    Rename to ocflib, add setup.py

---
+++ setup.py
@@ -0,0 +1,13 @@
+from setuptools import setup, find_packages
+
+VERSION = 1
+
+setup(
+    name='ocflib',
+    version=VERSION,
+    author="Open Computing Facility",
+    author_email='help@ocf.berkeley.edu',
+    description="libraries for account and server management",
+    url='https://www.ocf.berkeley.edu',
+    packages=find_packages()
+)
