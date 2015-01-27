40fc430b8686879c1cf528872024386d290a59e2
Chris Kuehl <ckuehl@ocf.berkeley.edu>
2015-01-27 13:23:30 -0800

    account/utils: add extract_username_from_principal

--- ocflib/account/utils.py
+++ ocflib/account/utils.py
@@ -1,4 +1,6 @@
 """Random account methods that don't fit anywhere else."""
+import re
+
 import pexpect
 import requests
 
@@ -36,3 +38,22 @@ def has_vhost(user):
 
     vhosts = requests.get(constants.VHOST_DB_URL).text.split("\n")
     return any(line_matches(line.split()) for line in vhosts)
+
+
+def extract_username_from_principal(principal):
+    """Extract username from principal.
+
+    >>> extract_username("ckuehl@OCF.BERKELEY.EDU")
+    'ckuehl'
+
+    >>> extract_username("ckuehl/admin@OCF.BERKELEY.EDU")
+    'ckuehl'
+    """
+
+    REGEX = '^([a-z]{3,8})(/[a-z]*)?@OCF.BERKELEY.EDU$'
+    match = re.match(REGEX, principal)
+
+    if not match:
+        raise ValueError("Invalid username")
+
+    return match.group(1)
