560e5a353c0ea9342a9c7418821e5064eb71990f
Chris Kuehl <ckuehl@ocf.berkeley.edu>
2015-01-28 19:40:00 -0800

    misc/mail: add method to mail user directly

--- ocflib/misc/mail.py
+++ ocflib/misc/mail.py
@@ -4,8 +4,24 @@ import email.mime.text
 import subprocess
 from email.utils import parseaddr
 
+import ocflib.account.validators
 import ocflib.constants as constants
 import ocflib.misc.validators as validators
+
+
+def email_for_user(username):
+    """Return email for a user.
+
+    Currently, just appends @ocf.berkeley.edu, but could eventually do
+    something more complicated."""
+
+    ocflib.account.validators.validate_username(username, check_exists=True)
+    return "{}@ocf.berkeley.edu".format(username)
+
+
+def send_mail_user(user, subject, body, sender=constants.MAIL_FROM):
+    """Send a plan-text mail message to a user."""
+    send_mail(email_for_user(user), subject, body, sender=sender)
 
 
 def send_mail(to, subject, body, sender=constants.MAIL_FROM):
