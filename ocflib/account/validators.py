e67eaa033c56e46d574895d22ea39e966fe7c3cc
Chris Kuehl <ckuehl@ocf.berkeley.edu>
2015-02-11 19:45:29 -0800

    account/validators: add local user sanity check

--- ocflib/account/validators.py
+++ ocflib/account/validators.py
@@ -1,10 +1,12 @@
 import difflib
 import pwd
 import string
+import sys
 
 import cracklib
 
 import ocflib.constants as constants
+import ocflib.misc.mail as mail
 
 
 def validate_username(username, check_exists=False):
@@ -63,6 +65,20 @@ def user_exists(username):
 def username_reserved(username):
     if username.startswith('ocf'):
         return True
+
+    # sanity check: make sure no local users share the username
+    with open('/etc/passwd') as f:
+        if any(line.startswith(username + ':') for line in f):
+            print(
+                "WARNING: Username {} rejected based on /etc/passwd!"
+                .format(username),
+                file=sys.stderr)
+            mail.send_problem_report(
+                """Username {} rejected based on /etc/passwd. It should be \
+added to RESERVED_USERNAMES for consistency across \
+servers!""".format(username))
+            return True
+
     return username in constants.RESERVED_USERNAMES
 
 
