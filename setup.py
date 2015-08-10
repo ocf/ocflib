a4860d26a2325bdf276598ccca01deb5531e0fbc
Chris Kuehl <ckuehl@ocf.berkeley.edu>
2015-08-09 23:37:47 -0700

    Add RC files to package_data, avoid broken pexpect

--- setup.py
+++ setup.py
@@ -12,13 +12,18 @@ setup(
     description='libraries for account and server management',
     url='https://www.ocf.berkeley.edu',
     packages=find_packages(),
+    package_data={
+        'ocflib.account': ['rc/*'],
+    },
     install_requires=(
         'colorama',
         'cracklib',
         'dnspython3',
         'ldap3',
         'paramiko',
-        'pexpect',
+        # pexpect 3.3 (currently the latest) breaks with multiprocessing/celery
+        # https://github.com/pexpect/pexpect/issues/86
+        'pexpect<3.2.999',
         'pycrypto',
         'pysnmp',
         'requests',
