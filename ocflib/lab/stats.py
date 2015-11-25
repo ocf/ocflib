d9a805bb78b9425e1102b9b2246256936dd015a7
Chris Kuehl <ckuehl@ocf.berkeley.edu>
2015-11-24 17:53:41 -0800

    Revert "Drop cached_property because of pip bug"
    This reverts commit 23a4377c77e857843e6e7cccdce305211f121425.
    
    @pydanny released a new version (1.3.0) of cached-property:
    https://github.com/pydanny/cached-property/pull/20#issuecomment-159461998
    
    Also closing rt#4160:
    https://rt.ocf.berkeley.edu/Ticket/Display.html?id=4160

--- ocflib/lab/stats.py
+++ ocflib/lab/stats.py
@@ -3,6 +3,7 @@ from collections import namedtuple
 from datetime import timedelta
 
 import pymysql
+from cached_property import cached_property
 
 from ocflib.constants import OCF_LDAP_HOSTS
 from ocflib.infra.ldap import ldap_ocf
@@ -101,12 +102,12 @@ class UtilizationProfile(namedtuple('UtilizationProfile', [
     def in_use(self, when):
         return any(s[0] <= when and (not s[1] or when <= s[1]) for s in self.sessions)
 
-    @property
+    @cached_property
     def total_minutes(self):
         """The total number of minutes captured by this profile."""
         return (self.end - self.start).total_seconds() // 60
 
-    @property
+    @cached_property
     def minutes_busy(self):
         """The number of minutes the computer was busy."""
         minutes_busy = 0
@@ -120,7 +121,7 @@ class UtilizationProfile(namedtuple('UtilizationProfile', [
 
         return minutes_busy
 
-    @property
+    @cached_property
     def minutes_idle(self):
         """The number of minutes the computer was idle."""
         return self.total_minutes - self.minutes_busy
