4fbdcb6023c469723a47fd2d81216b71d3c8fc41
Jason Perrin <jvperrin@ocf.berkeley.edu>
2016-07-27 11:34:23 -0700

    Add holidays for Fall 2016

--- ocflib/lab/hours.py
+++ ocflib/lab/hours.py
@@ -92,10 +92,10 @@ REGULAR_HOURS = defaultdict(lambda: [Hour(time(9), time(21))], {
 
 HOLIDAYS = [
     # start date, end date, holiday name, list of hours (date ranges are inclusive)
-    (date(2015, 12, 19), date(2016, 1, 18), 'Winter Break', []),
-    (date(2016, 2, 15), date(2016, 2, 15), 'President\'s Day', []),
-    (date(2016, 3, 18), date(2016, 3, 18), 'Friday before Spring Break', [Hour(time(9), time(12))]),
-    (date(2016, 3, 19), date(2016, 3, 27), 'Spring Break', []),
-    (date(2016, 5, 13), date(2016, 5, 13), 'Last Day Spring 2016', [Hour(time(9), time(12))]),
     (date(2016, 5, 14), date(2016, 8, 23), 'Summer Break', []),
+    (date(2016, 9, 5), date(2016, 9, 5), 'Labor Day', []),
+    (date(2016, 11, 11), date(2016, 11, 11), 'Veteran\'s Day', []),
+    (date(2016, 11, 23), date(2016, 11, 27), 'Thanksgiving Break', []),
+    (date(2016, 12, 16), date(2016, 12, 16), 'Last Day of Fall 2016', [Hour(time(9), time(12))]),
+    (date(2016, 12, 17), date(2017, 1, 16), 'Winter Break', []),
 ]
