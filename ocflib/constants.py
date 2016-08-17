a682bf6ea6c88edfa5004f2482d63cdb54818f27
Chris Kuehl <ckuehl@ocf.berkeley.edu>
2016-08-17 12:56:35 -0700

    Update CURRENT_SEMESTER_START

--- ocflib/constants.py
+++ ocflib/constants.py
@@ -1,7 +1,7 @@
 import datetime
 
 # update once a semester
-CURRENT_SEMESTER_START = datetime.date(2016, 1, 1)
+CURRENT_SEMESTER_START = datetime.date(2016, 8, 1)
 
 # ocf ldap
 OCF_LDAP = 'ldap.ocf.berkeley.edu'
