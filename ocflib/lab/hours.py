ddc1c5e8be4ff8af95835e8c077e906a45fe4a8e
Matthew McAllister <matthew.mcallister@berkeley.edu>
2016-10-16 19:16:35 -0700

    lab.hours: lost another pubstaff

--- ocflib/lab/hours.py
+++ ocflib/lab/hours.py
@@ -86,18 +86,16 @@ class Day(namedtuple('Day', ['date', 'weekday', 'holiday', 'hours'])):
 REGULAR_HOURS = defaultdict(lambda: [Hour(time(9), time(20))], {
     Day.MONDAY: [Hour(time(9), time(12)), Hour(time(14), time(15)), Hour(time(16), time(18))],
     Day.TUESDAY: [Hour(time(9), time(10)), Hour(time(13), time(18))],
-    Day.WEDNESDAY: [Hour(time(9), time(12)), Hour(time(13), time(14)), Hour(time(18), time(19))],
+    Day.WEDNESDAY: [Hour(time(9), time(12)), Hour(time(13), time(14)),
+                    Hour(time(15), time(16)), Hour(time(18), time(19))],
     Day.THURSDAY: [Hour(time(9), time(10)), Hour(time(13), time(19))],
     Day.FRIDAY: [Hour(time(9), time(12)), Hour(time(14), time(18))],
     Day.SATURDAY: [Hour(time(12), time(17))],
-    Day.SUNDAY: [Hour(time(12), time(17))],
+    Day.SUNDAY: [Hour(time(12), time(14))],
 })
 
 HOLIDAYS = [
     # start date, end date, holiday name, list of hours (date ranges are inclusive)
-    (date(2016, 5, 14), date(2016, 8, 23), 'Summer Break', []),
-    (date(2016, 9, 5), date(2016, 9, 5), 'Labor Day', []),
-    (date(2016, 9, 7), date(2016, 9, 7), 'General Meeting', [Hour(time(9), time(18))]),
     (date(2016, 11, 11), date(2016, 11, 11), 'Veteran\'s Day', []),
     (date(2016, 11, 23), date(2016, 11, 27), 'Thanksgiving Break', []),
     (date(2016, 12, 16), date(2016, 12, 16), 'Last Day of Fall 2016', [Hour(time(9), time(12))]),
