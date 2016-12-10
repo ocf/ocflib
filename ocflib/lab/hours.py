9b43bbcff26e36009e52d1e03f37fd41e43f0e5e
Nick Impicciche <nick.impicciche@gmail.com>
2016-12-10 01:56:13 -0800

    Why am I so bad at just inputting the whole schedule

--- ocflib/lab/hours.py
+++ ocflib/lab/hours.py
@@ -86,6 +86,7 @@ class Day(namedtuple('Day', ['date', 'weekday', 'holiday', 'hours'])):
 REGULAR_HOURS = defaultdict(lambda: [Hour(time(9), time(19))], {
     Day.MONDAY: [Hour(time(9), time(18))],
     Day.TUESDAY: [Hour(time(11), time(15))],
+    Day.SUNDAY: [Hour(time(12), time(17))],
 })
 
 HOLIDAYS = [
