00f1a9fa682c1e692b370325a56a9d33d89c4578
Nick Impicciche <nick.impicciche@gmail.com>
2017-01-21 16:08:08 -0800

    Update hours.py

--- ocflib/lab/hours.py
+++ ocflib/lab/hours.py
@@ -84,12 +84,13 @@ class Day(namedtuple('Day', ['date', 'weekday', 'holiday', 'hours'])):
 
 
 REGULAR_HOURS = defaultdict(lambda: [Hour(time(9), time(19))], {
-    Day.MONDAY: [Hour(time(9), time(18))],
-    Day.TUESDAY: [Hour(time(11), time(14)), Hour(time(15), time(19))],
-    Day.THURSDAY: [Hour(time(11), time(14)), Hour(time(15), time(16)), Hour(time(17), time(19))],
+    Day.MONDAY: [Hour(time(9), time(16)), Hour(time(17), time(18))],
+    Day.TUESDAY: [Hour(time(11), time(19))],
+    Day.WEDNESDAY: [Hour(time(9), time(16)), Hour(time(18), time(19))],
+    Day.THURSDAY: [Hour(time(11), time(15)), Hour(time(16), time(19))],
     Day.FRIDAY: [Hour(time(9), time(18))],
-    Day.SATURDAY: [Hour(time(12), time(17))],
-    Day.SUNDAY: [Hour(time(12), time(17))],
+    Day.SATURDAY: [Hour(time(12), time(16))],
+    Day.SUNDAY: [Hour(time(12), time(16))],
 })
 
 HOLIDAYS = [
