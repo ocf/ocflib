ebed26fdc9a09326ee67f16ac4d3883bf6753db3
Matthew McAllister <matthew.mcallister.0@gmail.com>
2017-10-04 12:57:51 -0700

    lab.stats: Use A instead of CNAME for staffdesk

--- ocflib/lab/stats.py
+++ ocflib/lab/stats.py
@@ -142,7 +142,7 @@ def list_desktops(public_only=False):
     if not public_only:
         filter = '(type=desktop)'
     else:
-        filter = '(&(type=desktop)(!(|(dnsCname=frontdesk)(dnsCname=staffdesk))))'
+        filter = '(&(type=desktop)(!(|(dnsCname=frontdesk)(dnsA=staffdesk))))'
 
     with ldap_ocf() as c:
         c.search(OCF_LDAP_HOSTS, filter, attributes=['cn'])
