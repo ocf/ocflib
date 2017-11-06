3e770f87a91a227a2c67de4a49e1fa1f9aba9b7e
Tony Situ <c2.tonyc2@gmail.com>
2017-11-06 15:56:43 -0800

    added function for key existence check, and collision checking

--- ocflib/api/keys.py
+++ ocflib/api/keys.py
@@ -17,6 +17,76 @@ def _generate_key(length=KEY_LENGTH):
     return hexlify(urandom(length)).decode()
 
 
+def get_key(cursor, user):
+    """Get the key corresponding to the user in the db
+
+    Args:
+        cursor (pymysql.cursors.Cursor): database cursor
+        user (str): user to get the key for
+
+    Returns:
+        (str) the user's key, or None if there is no matching row
+    """
+    cursor.execute(
+        'SELECT `key`'
+        'FROM `keys`'
+        'WHERE `user` LIKE %s',
+        (user,),
+    )
+    query_result = cursor.fetchone()
+
+    try:
+        return query_result['key']
+    except (KeyError, TypeError):
+        # No matching row
+        return None
+
+def get_user(cursor, key):
+    """Get the user corresponding to the key in the db
+
+    Args:
+        cursor (pymysql.cursors.Cursor): database cursor
+        key (str): key that corresponds to the user
+
+    Returns:
+        (str) user that owns the key or None if there is no matching row
+    """
+    cursor.execute(
+        'SELECT `user`'
+        'FROM `keys`'
+        'WHERE `key` LIKE %s',
+        (key,),
+    )
+    query_result = cursor.fetchone()
+
+
+    try:
+        return query_result['user']
+    except (KeyError, TypeError):
+        # No matching row
+        return None
+
+def key_exists(cursor, key):
+    """Checks if the key currently exists in the db
+
+    Args:
+        cursor (pymysql.cursors.Cursor): database cursor
+        key (str): key to check for
+
+    Returns:
+        (bool) whether or not the key exists
+    """
+    cursor.execute(
+        'SELECT 1'
+        'FROM `keys`'
+        'WHERE `key` LIKE %s',
+        (key,),
+    )
+    query_result = cursor.fetchone()
+
+    return query_result is not None
+
+
 def add_key(cursor, user):
     """Add a key for a user into the database
 
@@ -28,6 +98,11 @@ def add_key(cursor, user):
         None
     """
     key = _generate_key()
+
+    # Re-generate key in case of collision
+    while key_exists(key):
+        key = _generate_key()
+
     cursor.execute(
         'INSERT INTO `keys`'
         '(`key`, `user`)'
@@ -35,31 +110,6 @@ def add_key(cursor, user):
         (key, user)
     )
 
-
-def get_key(cursor, user):
-    """Get the key corresponding to the user in the db
-
-    Args:
-        cursor (pymysql.cursors.Cursor): database cursor
-        user (str): user to get the key for
-
-    Returns:
-        (str) the user's key
-    """
-    cursor.execute(
-        'SELECT `key`'
-        'FROM `keys`'
-        'WHERE `user` LIKE %s',
-        (user,),
-    )
-    query_result = cursor.fetchone()
-    try:
-        return query_result['key']
-    except (KeyError, TypeError):
-        # query_result is None or doesn't have a key
-        return ""
-        
-    return cursor.fetchone()['key']
 
 def get_connection(user, password, db='keys', **kwargs):
     """Return a connection to MySQL."""
