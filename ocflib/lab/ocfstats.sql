e5d67ff23209c807f0089c60a954a4f6877ca281
Jason Perrin <jvperrin@ocf.berkeley.edu>
2017-11-23 17:14:18 -0800

    Remove views and only create absent tables

--- ocflib/lab/ocfstats.sql
+++ ocflib/lab/ocfstats.sql
@@ -2,7 +2,7 @@
 --- Documentation of the ocfstats database schema
 ---
 
-CREATE TABLE `session` (
+CREATE TABLE IF NOT EXISTS `session` (
     `id` int NOT NULL AUTO_INCREMENT,
     `host` varchar(255) NOT NULL,
     `user` varchar(16) NOT NULL,
@@ -12,17 +12,17 @@ CREATE TABLE `session` (
     PRIMARY KEY (`id`)
 ) ENGINE=InnoDB;
 
-CREATE TABLE `staff` (
+CREATE TABLE IF NOT EXISTS `staff` (
     `user` varchar(16) NOT NULL,
     PRIMARY KEY (`user`)
 ) ENGINE=InnoDB;
 
-CREATE TABLE `opstaff` (
+CREATE TABLE IF NOT EXISTS `opstaff` (
     `user` varchar(16) NOT NULL,
     PRIMARY KEY (`user`)
 ) ENGINE=InnoDB;
 
-CREATE TABLE `printer_pages` (
+CREATE TABLE IF NOT EXISTS `printer_pages` (
     `id` int(11) NOT NULL AUTO_INCREMENT,
     `date` datetime NOT NULL,
     `printer` varchar(255) NOT NULL,
@@ -30,7 +30,7 @@ CREATE TABLE `printer_pages` (
     PRIMARY KEY (`id`)
 ) ENGINE=InnoDB;
 
-CREATE TABLE `printer_toner` (
+CREATE TABLE IF NOT EXISTS `printer_toner` (
     `id` int(11) NOT NULL AUTO_INCREMENT,
     `date` datetime NOT NULL,
     `printer` varchar(255) NOT NULL,
@@ -39,46 +39,57 @@ CREATE TABLE `printer_toner` (
     PRIMARY KEY (`id`)
 ) ENGINE=InnoDB;
 
-CREATE TABLE `mirrors` (
+CREATE TABLE IF NOT EXISTS `mirrors` (
     `date` date NOT NULL,
     `dist` varchar(30) NOT NULL,
     `up` bigint(20) NOT NULL,
     `down` bigint(20) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 
+
+DROP VIEW IF EXISTS session_duration;
 CREATE VIEW `session_duration` AS
     SELECT *, timediff(`end`, `start`) AS `duration` FROM `session`;
 
+DROP VIEW IF EXISTS session_duration_public;
 CREATE VIEW `session_duration_public` AS
     SELECT `id`, `host`, `start`, `end`, `duration` FROM `session_duration`;
 
+DROP VIEW IF EXISTS staff_session_duration_public;
 CREATE VIEW `staff_session_duration_public` AS
     SELECT * FROM `session_duration` WHERE `user` IN (
         SELECT `user` FROM `staff`
     );
 
+DROP VIEW IF EXISTS users_in_lab;
 CREATE VIEW `users_in_lab` AS
     SELECT `user`, `host`, `start` FROM `session` WHERE `end` IS NULL;
 
+DROP VIEW IF EXISTS users_in_lab_count_public;
 CREATE VIEW `users_in_lab_count_public` AS
     SELECT COUNT(DISTINCT `user`) AS `count` FROM `users_in_lab`;
 
 --- This relies on the semester_start function, which is defined in ocfprinting.sql
+DROP VIEW IF EXISTS unique_users_in_lab_count_public;
 CREATE VIEW `unique_users_in_lab_count_public` AS
     SELECT COUNT(DISTINCT `user`) AS `users` FROM `session`
         WHERE `start` >= `semester_start`(CURDATE());
 
+DROP VIEW IF EXISTS staff_in_lab_public;
 CREATE VIEW `staff_in_lab_public` AS
     SELECT * FROM `users_in_lab` WHERE `user` IN (
         SELECT `user` FROM `staff`
     );
 
+DROP VIEW IF EXISTS printer_pages_public;
 CREATE VIEW `printer_pages_public` AS
     SELECT `id`, `date`, `printer`, `value` FROM `printer_pages`;
 
+DROP VIEW IF EXISTS printer_toner_public;
 CREATE VIEW `printer_toner_public` AS
     SELECT `id`, `date`, `printer`, `value`, `max` FROM `printer_toner`;
 
+DROP VIEW IF EXISTS daily_sessions_public;
 CREATE VIEW `daily_sessions_public` AS
     SELECT
         COUNT(*) as logins,
@@ -88,6 +99,7 @@ CREATE VIEW `daily_sessions_public` AS
         GROUP BY `date`
         ORDER BY `date` DESC;
 
+DROP VIEW IF EXISTS mirrors_public;
 CREATE VIEW `mirrors_public` AS
     SELECT * FROM `mirrors`;
 
