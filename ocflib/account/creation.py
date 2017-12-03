6c4fd9f0f8b9c0d9aad1d53e66f9c01a287216ec
Tony Liu <trliu@ocf.berkeley.edu>
2017-12-02 16:34:00 -0800

    Corrected formatting

--- ocflib/account/creation.py
+++ ocflib/account/creation.py
@@ -29,11 +29,13 @@ from ocflib.printing.quota import SEMESTERLY_QUOTA
 
 
 _KNOWN_UID = 50500
-BAD_WORDS = frozenset(('anal', 'anus', 'arse', 'ass', 'ballsack', 'balls', 'bastard', 'bitch', 'biatch', 'bloody',
-'blowjob', 'bollock', 'bollok', 'boner', 'boob', 'chink', 'clitoris', 'cock', 'coon', 'crap', 'cunt', 'damn', 'dick',
-'dildo', 'douche', 'dyke', 'fag', 'feck', 'fellate', 'fellatio', 'felching', 'fuck', 'flange', 'hell', 'homo', 'jerk',
-'jizz', 'kike', 'knobend', 'labia', 'muff', 'nigger', 'nigga', 'penis', 'piss', 'prick', 'pube', 'pussy', 'queer',
-'scrotum', 'sex', 'shit', 'slut', 'smegma', 'terrorist', 'tit', 'twat', 'vagina', 'wank', 'whore'))
+BAD_WORDS = frozenset((
+    'anal', 'anus', 'arse', 'ass', 'ballsack', 'balls', 'bastard', 'bitch', 'biatch', 'bloody', 'blowjob', 'bollock',
+    'bollok', 'boner', 'boob', 'chink', 'clitoris', 'cock', 'coon', 'crap', 'cunt', 'damn', 'dick', 'dildo', 'douche',
+    'dyke', 'fag', 'feck', 'fellate', 'fellatio', 'felching', 'fuck', 'flange', 'hell', 'homo', 'jerk', 'jizz', 'kike',
+    'knobend', 'labia', 'muff', 'nigger', 'nigga', 'penis', 'piss', 'prick', 'pube', 'pussy', 'queer', 'scrotum',
+    'sex', 'shit', 'slut', 'smegma', 'terrorist', 'tit', 'twat', 'vagina', 'wank', 'whore'
+))
 RESTRICTED_WORDS = frozenset(('ocf', 'ucb', 'cal', 'berkeley', 'university'))
 
 CREATE_PUBLIC_KEY = '''\
