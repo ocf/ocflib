ac69e56dfc9772a874918d81deaff7a5d19fcf8d
Jason Perrin <jvperrin@ocf.berkeley.edu>
2018-02-27 15:49:17 -0800

    Change ocfmail forward_to to text type
    This should prevent the kind of problems in rt#7140 where addresses get
    cut off silently and cause problems.

--- ocflib/vhost/ocfmail.sql
+++ ocflib/vhost/ocfmail.sql
@@ -3,7 +3,7 @@ ALTER DATABASE CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
 CREATE TABLE IF NOT EXISTS `addresses` (
   `address` varchar(190) NOT NULL,
   `password` varchar(255) DEFAULT NULL,
-  `forward_to` varchar(255) NOT NULL,
+  `forward_to` text NOT NULL,
   `last_updated` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP
       ON UPDATE CURRENT_TIMESTAMP,
   PRIMARY KEY(`address`)
