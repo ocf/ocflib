1f1cddcfcffe857fd50ff9fceb4fdab26be180c7
Daniel Kessler <dkessler@berkeley.edu>
2018-09-11 10:32:17 -0700

    move paren

--- ocflib/infra/discourse.py
+++ ocflib/infra/discourse.py
@@ -38,8 +38,7 @@ class DiscourseTopic(namedtuple('DiscourseTopic', ('number', 'title', 'starter',
         category_id = topic['category_id']
 
         cat_resp = requests.get('{}/categories.json'.format(DISCOURSE_ROOT),
-                                params=params
-                                )
+                                params=params)
         if cat_resp.status_code != 200:
             raise DiscourseError(
                 'Category request gave {}'.format(cat_resp.status_code))
