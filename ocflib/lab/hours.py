6403193f03fca1c2d281e6f596761766320b26e9
Daniel Kessler <dkessler@ocf.berkeley.edu>
2018-09-11 23:19:39 -0700

    Add early lab closure for fb linux challenge

--- ocflib/lab/hours.py
+++ ocflib/lab/hours.py
@@ -196,6 +196,7 @@ HOLIDAYS = [
     (date(2018, 5, 12), date(2018, 8, 21), 'Summer Break', []),
     (date(2018, 9, 1), date(2018, 9, 3), 'Labor Day', []),
     (date(2018, 9, 12), date(2018, 9, 12), 'Early Lab Closure', [Hour(time(9), time(19))]),
+    (date(2018, 9, 18), date(2018, 9, 18), 'Early Lab Closure', [Hour(time(9), time(18))]),
     (date(2018, 11, 12), date(2018, 11, 12), 'Veterans Day', []),
     (date(2018, 11, 21), date(2018, 11, 25), 'Thanksgiving Break', []),
     (date(2018, 12, 15), date(2019, 1, 14), 'Winter Break', []),
