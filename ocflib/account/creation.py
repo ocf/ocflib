8c63e5f142c9c70b3f68d0ca1ccb003fa06a30b8
Daniel Kessler <dkessler@ocf.berkeley.edu>
2018-09-18 14:00:33 -0700

    Bump known uid

--- ocflib/account/creation.py
+++ ocflib/account/creation.py
@@ -28,7 +28,7 @@ from ocflib.misc.validators import valid_email
 from ocflib.printing.quota import SEMESTERLY_QUOTA
 
 
-_KNOWN_UID = 53500
+_KNOWN_UID = 56162
 BAD_WORDS = frozenset((
     'anal', 'anus', 'arse', 'ass', 'bastard', 'bitch', 'biatch', 'bloody', 'blowjob', 'bollock',
     'bollok', 'boner', 'chink', 'clit', 'cock', 'coon', 'cunt', 'damn', 'dick', 'dildo', 'douche',
