be0fbd106ee4bdb6693e2109aae472b081305a08
Daniel Kessler <dkessler@ocf.berkeley.edu>
2018-10-30 10:14:50 -0700

    Update Callink API groups endpoint

--- ocflib/ucb/groups.py
+++ ocflib/ucb/groups.py
@@ -9,7 +9,7 @@ from ocflib.ucb.directory import name_by_calnet_uid
 
 
 _API = {
-    'BASE': 'https://studentservices.berkeley.edu/WebServices/StudentGroupServiceV2/Service.asmx',
+    'BASE': 'https://orapps.berkeley.edu/StudentGroupServiceV2/service.asmx',
     'SERVICE': {
         'ORGS': 'CalLinkOrganizations',
         'SIGNATORIES_BY_OID': 'CalLinkGroupSignatories',
