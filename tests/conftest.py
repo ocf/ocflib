63921a069a9f8f36550209572db3b24a7c7be901
Frank Dai <fydai@ocf.berkeley.edu>
2019-06-04 23:22:24 -0700

    Change jvperrin's expired Calnet to fydai's

--- tests/conftest.py
+++ tests/conftest.py
@@ -16,8 +16,8 @@ import pytest
 # CalNet UID. Alumni eventually get kicked out of the university's
 # "People" OU, so these constants have to be updated every few years
 # after the aforementioned party has graduated.
-TEST_PERSON_CALNET_UID = 1101587
-TEST_PERSON_NAME = 'Jason Perrin'
+TEST_PERSON_CALNET_UID = 1538112
+TEST_PERSON_NAME = 'Frank Dai'
 
 
 MYSQL_TIMEOUT = 10
