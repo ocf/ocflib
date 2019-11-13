2ec4a9e7168ab804ae9384553952a3c6c93d13ad
Christopher Cooper <cooperc@ocf.berkeley.edu>
2019-11-12 16:14:53 -0800

    Revert "Temporary fix to make password resets work (actually this time)"
    This reverts commit b93495dd8328ec6aaad58f0d92a8a10bc9f3b7f2.

--- ocflib/ucb/groups.py
+++ ocflib/ucb/groups.py
@@ -148,8 +148,7 @@ def _get_osl(query, service, parser):
     url = '{}/{}?{}'.format(_API['BASE'], service, urlencode(query))
 
     r = requests.get(url, timeout=20)
-    # HACKY TEMPORARY SOLUTION UNTIL THEY FIX THEIR API
-    return {}
+    return _parse_osl(ElementTree.fromstring(r.text), parser)
 
 
 def _parse_osl(root, parser):
