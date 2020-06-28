136871841fa7e607c64e3266e57f61163a66edf2
Ja (Thanakul) Wattanawong <jaw@ocf.berkeley.edu>
2020-06-27 20:04:22 -0700

    Add feature to PR new vhosts to GitHub

--- ocflib/vhost/web.py
+++ ocflib/vhost/web.py
@@ -1,15 +1,104 @@
 import re
+import uuid
+from collections import namedtuple
+from datetime import date
+from textwrap import dedent
 
 from ocflib.account.search import user_attrs
 from ocflib.account.search import user_attrs_ucb
+from ocflib.infra.github import get_github_file
+from ocflib.infra.github import get_repo
+from ocflib.infra.github import modify_and_branch
+from ocflib.infra.rt import rt_connection
+from ocflib.infra.rt import RtTicket
 
 VHOST_DB_PATH = '/etc/ocf/vhost.conf'
+GITHUB_VHOST_REPO = 'ocf/etc'
+GITHUB_VHOST_WEB_PATH = 'configs/vhost.conf'
 
 
-def get_vhost_db():
-    """Returns lines from the vhost config file."""
-    with open(VHOST_DB_PATH) as f:
-        return f.read().splitlines()
+def get_vhost_db(remote=False):
+    """Returns lines from the vhost config file. If remote is True, fetches it
+    from GitHub."""
+    if remote:
+        vhosts = get_github_file(GITHUB_VHOST_REPO, GITHUB_VHOST_WEB_PATH)
+        return vhosts.splitlines()
+    else:
+        with open(VHOST_DB_PATH) as f:
+            return f.read().splitlines()
+
+
+def pr_new_vhost(credentials, username, aliases=None, docroot=None, flags=None, rt_ticket=None):
+    'Creates a GitHub pull request on the vhosts file to add a new vhost'
+    if not aliases:
+        aliases = '-'
+    else:
+        aliases = ','.join(aliases)
+
+    if not docroot:
+        docroot = '-'
+
+    if not flags:
+        flags = '-'
+
+    repo = get_repo(GITHUB_VHOST_REPO, credentials=credentials)
+    vhosts_lines = get_vhost_db(remote=True)
+
+    # skip the initial comment block
+    idx = 0
+    while idx < len(vhosts_lines):
+        if not vhosts_lines[idx].startswith('#'):
+            break
+        idx += 1
+
+    vhosts_lines.insert(idx, dedent("""
+        # added {date} web rt#{rt_ticket}
+        {username} {aliases} {docroot} {flags}""").format(
+        date=date.today(),
+        rt_ticket=rt_ticket,
+        username=username,
+        aliases=aliases,
+        docroot=docroot,
+        flags=flags,
+
+    ))
+
+    new_vhosts_file = '\n'.join(vhosts_lines) + '\n'  # newline at eof
+    new_branch_name = uuid.uuid4().hex
+
+    modify_and_branch(repo, 'master', new_branch_name,
+                      'rt#{rt_ticket}: Add vhost for {username}'.format(
+                          rt_ticket=rt_ticket,
+                          username=username,
+                      ), GITHUB_VHOST_WEB_PATH, new_vhosts_file)
+
+    pull_body = dedent("""
+        Submitted from ocflib on {date}
+
+        Username: {username}
+        Aliases: {aliases}
+        Document root: {docroot}
+        Flags: {flags}
+
+        Associated RT Ticket: {rt_ticket}
+        """).format(
+        date=date.today(),
+        rt_ticket=rt_ticket,
+        username=username,
+        aliases=aliases,
+        docroot=docroot,
+        flags=flags,
+    )
+
+    repo.create_pull(
+        title='rt#{rt_ticket}: Add vhost for {username}'.format(
+            rt_ticket=rt_ticket,
+            username=username,
+        ),
+        body=pull_body,
+        base='master',
+        head=new_branch_name,
+    )
 
 
 def get_vhosts():
@@ -84,3 +173,66 @@ def eligible_for_vhost(user):
             return True
 
     return False
+
+
+def get_tasks(celery_app, credentials=None):
+    """Return Celery tasks instantiated against the provided instance."""
+
+    @celery_app.task
+    def create_new_vhost(request):
+        try:
+            conn = rt_connection(credentials['rt'].username, credentials['rt'].password)
+            ticket = RtTicket.create(
+                conn,
+                'hostmaster',
+                request.requestor,
+                request.subject,
+                request.message,
+            )
+
+            pr_new_vhost(
+                credentials['github'],
+                request.username,
+                request.aliases,
+                request.docroot,
+                request.flags,
+                ticket.number,
+            )
+            return True
+        except Exception:
+            # TODO: Report errors via ocflib
+            return False
+
+    return _VirtualHostTasks(
+        create_new_vhost=create_new_vhost,
+    )
+
+
+_VirtualHostTasks = namedtuple('VirtualHostTasks', [
+    'create_new_vhost',
+])
+
+
+class NewVirtualHostRequest(namedtuple('NewVirtualHostRequest', [
+    'username',
+    'requestor',
+    'subdomain',
+    'aliases',
+    'docroot',
+    'flags',
+    'rt_ticket',
+    'subject',
+    'message',
+])):
+    """Request for account creation.
+
+    :param username: str
+    :param requestor: str
+    :param subdomain: str
+    :param aliases: str or None
+    :param docroot: str or None
+    :param flags: str or None
+    :param rt_ticket: str or None
+    :param subject: str or None
+    :param message: str or None
+    """
--- tests/vhost/web_test.py
+++ tests/vhost/web_test.py
@@ -1,11 +1,15 @@
 import mock
 import pytest
 
+from ocflib.infra.github import GithubCredentials
+from ocflib.infra.rt import RtCredentials
 from ocflib.vhost.web import eligible_for_vhost
+from ocflib.vhost.web import get_tasks
 from ocflib.vhost.web import get_vhost_db
 from ocflib.vhost.web import get_vhosts
 from ocflib.vhost.web import has_vhost
-
+from ocflib.vhost.web import NewVirtualHostRequest
+from tests.fixtures_test import celery_app  # noqa
 
 VHOSTS_EXAMPLE = """
 # added 2017-09-16 kpengboy
@@ -80,3 +84,63 @@ class TestVirtualHosts:
     ])
     def test_eligible_for_vhost(self, user, should_be_eligible):
         assert eligible_for_vhost(user) == should_be_eligible
+
+
+@pytest.yield_fixture
+def fake_credentials():
+    yield {
+        'rt': RtCredentials(username='ocf', password='password'),
+        'github': GithubCredentials(token='ocf')
+    }
+
+
+@pytest.yield_fixture
+def tasks(celery_app, fake_credentials):
+    yield get_tasks(celery_app, fake_credentials)
+
+
+@pytest.yield_fixture
+def fake_new_vhost_request():
+    yield NewVirtualHostRequest(
+        'ocf',
+        'ocf@ocf.berkeley.edu',
+        'ocf.berkeley.edu',
+        None,
+        None,
+        None,
+        '0',
+        'vhost request for ocf',
+        'body',
+    )
+
+
+@pytest.yield_fixture
+def mock_rt_connection():
+    with mock.patch('ocflib.vhost.web.rt_connection') as m:
+        yield m
+
+
+@pytest.yield_fixture
+def mock_rtticket_create():
+    with mock.patch('ocflib.vhost.web.RtTicket.create') as m:
+        yield m
+
+
+@pytest.yield_fixture
+def mock_github():
+    with mock.patch('ocflib.infra.github.Github') as m:
+        yield m
+
+
+def test_create_new_vhost_successful(
+        celery_app,
+        mock_rt_connection,
+        mock_rtticket_create,
+        fake_new_vhost_request,
+        mock_github,
+        tasks):
+    resp = tasks.create_new_vhost(fake_new_vhost_request)
+    assert resp
+    assert mock_rt_connection.called
+    assert mock_rtticket_create.called
+    assert mock_github.called
