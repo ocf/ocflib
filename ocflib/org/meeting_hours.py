c0eabc77ffb08e6740d518d893ededc2fc1801d6
Ben Plate <bplate9583@gmail.com>
2021-10-20 21:06:18 -0700

    pep8 and import fixes

--- ocflib/org/meeting_hours.py
+++ ocflib/org/meeting_hours.py
@@ -7,24 +7,30 @@ from collections import namedtuple
 from datetime import date
 from datetime import datetime
 from datetime import timedelta
-
-import time
-import yaml
+from time import localtime
+from yaml import safe_load
 
 MEETING_HOURS_FILE = '/etc/ocf/meeting_hours.yaml'
+DAYS_OF_WEEK = ['Monday', 'Tuesday', 'Wednesday',
+                'Thursday', 'Friday', 'Saturday', 'Sunday']
 
-Meeting = namedtuple('Meeting', ['day', 'time', 'subject', 'short', 'irl', 'virtual'])
+Meeting = namedtuple(
+    'Meeting',
+    ['day', 'time', 'subject', 'short', 'irl', 'virtual']
+)
+
 
 def _load_meeting_hours():
     """Load meeting hours, from the /etc/ocf folder."""
     with open(MEETING_HOURS_FILE, 'r') as f:
-        return yaml.safe_load(f)
+        return safe_load(f)
+
 
 def _get_meeting_hours():
     meeting_hours = _load_meeting_hours()
 
     meeting_hour_list = []
-    for day in ['Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Saturday', 'Sunday']:
+    for day in DAYS_OF_WEEK:
         if not meeting_hours['meeting_hours'][day]:
             continue
 
@@ -42,13 +48,23 @@ def _get_meeting_hours():
 
     return meeting_hour_list
 
+
 def _parse_hour(hour):
     """
     Converts a 2-element list of hours like ['11:00', '13:00'] to a string
     in 12-hour time, like '11:00AM - 1:00PM'.
     """
-    return '{} - {}'.format(datetime.strptime(hour[0], '%H:%M').strftime('%-I:%M%p'),
-                            datetime.strptime(hour[1], '%H:%M').strftime('%-I:%M%p'))
+    return '{} - {}'.format(
+        datetime.strptime(
+            hour[0],
+            '%H:%M'
+        ).strftime('%-I:%M%p'),
+        datetime.strptime(
+            hour[1],
+            '%H:%M'
+        ).strftime('%-I:%M%p')
+    )
+
 
 def _time_to_range(hours):
     """
@@ -79,40 +95,57 @@ def _time_to_range(hours):
     end_hours = hours[hyphen+2:second_colon]
     end_minutes = hours[second_colon+1:second_colon+3]
 
-    start_time = (start_hours + (12 if first_half == 'PM' else 0)) * 60 + start_minutes
-    end_time = (end_hours + (12 if second_half == 'PM' else 0) * 60 + end_minutes)
+    start_pm_offset = 12 if first_half == 'PM' else 0
+    end_pm_offset = 12 if second_half == 'PM' else 0
+
+    start_time = start_hours + start_pm_offset * 60 + start_minutes
+    end_time = end_hours + end_pm_offset * 60 + end_minutes
 
     return (start_time, end_time)
 
+
 def _get_next_meeting():
     today = date.today()
-    now = time.localtime().tm_hour * 60 + time.localtime().tm_min
+    now = localtime().tm_hour * 60 + localtime().tm_min
     days = [(today + timedelta(days=i)).strftime('%A') for i in range(7)]
 
-    meetings = sorted(_get_meeting_hours(), key=lambda meeting: days.index(meeting.day))
+    meetings = sorted(
+        _get_meeting_hours(),
+        key=lambda meeting: days.index(meeting.day)
+    )
+
     for meeting in meetings:
         if _time_to_range(meeting.time)[0] > now:
             return meeting
 
     return None
 
+
 def _get_current_meeting():
     today = date.today()
-    now = time.localtime().tm_hour * 60 + time.localtime().tm_min
+    now = localtime().tm_hour * 60 + localtime().tm_min
     days = [(today + timedelta(days=i)).strftime('%A') for i in range(7)]
 
-    meetings = sorted(_get_meeting_hours(), key=lambda meeting: days.index(meeting.day))
+    meetings = sorted(
+        _get_meeting_hours(),
+        key=lambda meeting: days.index(meeting.day)
+    )
+
+    ranged_time = _time_to_range(meeting.time)
     for meeting in meetings:
-        if _time_to_range(meeting.time)[0] < now and _time_to_range(meeting.time)[1]:
+        if ranged_time[0] < now and ranged_time[1]:
             return meeting
 
     return None
 
+
 def read_meeting_list():
     return _get_meeting_hours()
 
+
 def read_next_meeting():
     return _get_next_meeting()
+
 
 def read_current_meeting():
     return _get_current_meeting()
