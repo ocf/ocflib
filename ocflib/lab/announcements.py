664b9ee0bb00600f5b177facda6c0cb2ba9347a4
Victor Shi <jieteng@berkeley.edu>
2023-12-04 17:27:24 -0800

    new cache implementation

--- ocflib/lab/announcements.py
+++ ocflib/lab/announcements.py
@@ -1,8 +1,6 @@
 """Announcements handling"""
-from collections import deque
 from collections import namedtuple
 from datetime import datetime
-from typing import Dict
 
 from requests import get
 from yaml import safe_load
@@ -11,11 +9,13 @@ from yaml import safe_load
 ANNOUNCEMENTS_URL = (
     'https://api.github.com/repos/ocf/announcements/contents/{folder}/{id}'
 )
-CACHE_LEN = 10
+# 1 day in seconds
+TIME_TO_LIVE = 60 * 60 * 24
 
 # post_cache is a dict of {id: post content}
-post_cache: Dict[str, str] = {}
-id_cache = deque(maxlen=CACHE_LEN)
+post_cache = {}
+last_updated = datetime.now()
+
 Metadata = namedtuple('Metadata', ['title', 'date', 'author', 'tags', 'summary'])
 
 
@@ -23,13 +23,25 @@ def _check_id(id: str) -> bool:
     """Check if the id is a valid date"""
 
     try:
-        datetime.strptime(id, '%Y-%m-%d-%M')  # TODO: if this %M (minute) is sufficient
+        datetime.strptime(id, '%Y-%m-%d-%M')
     except ValueError:
         raise ValueError('Invalid id')
 
 
+def _clear_cache() -> None:
+    """Clear the cache if it's too old"""
+
+    global last_updated
+    if (datetime.now() - last_updated).total_seconds() > TIME_TO_LIVE:
+        post_cache.clear()
+        last_updated = datetime.now()
+
+
 def get_all_announcements(folder='announcements') -> [dict]:
-    """Get announcements from the announcements repo"""
+    """
+    Get announcements from the announcements repo
+    The result is a list of post metadatas
+    """
 
     posts = get(
         url=ANNOUNCEMENTS_URL.format(folder=folder, id=''),
@@ -41,9 +53,14 @@ def get_all_announcements(folder='announcements') -> [dict]:
 
 
 def get_announcement(id: str, folder='announcements') -> str:
-    """Get one particular announcement from the announcements repo"""
+    """
+    Get one particular announcement from the announcements repo
+    The result is the post content
+    """
 
     _check_id(id)
+    # if the cache is too old, clear it
+    _clear_cache()
 
     if id in post_cache:
         return post_cache[id]
@@ -55,10 +72,6 @@ def get_announcement(id: str, folder='announcements') -> str:
     post.raise_for_status()
 
     post_cache[id] = post.text
-
-    # add the most recent id to the left of the deque
-    if id not in id_cache:
-        id_cache.appendleft(id)
 
     return post.text
 
@@ -82,21 +95,18 @@ def get_metadata(post_text: str) -> Metadata:
 
     try:
         meta_dict = safe_load(post_text.split('---')[1])
-    except IndexError:
-        raise IndexError('Missing metadata in announcement')
 
-    try:
-        metadata = Metadata(
+        data = Metadata(
             title=meta_dict['title'],
             date=meta_dict['date'],
             author=meta_dict['author'],
             tags=meta_dict['tags'],
             summary=meta_dict['summary'],
         )
-    except KeyError:
-        raise KeyError('Missing metadata in announcement')
+    except (IndexError, KeyError) as e:
+        raise ValueError(f'Error parsing metadata: {e}')
 
-    return metadata
+    return data
 
 
 def get_last_n_announcements_text(n: int) -> [str]:
@@ -106,19 +116,11 @@ def get_last_n_announcements_text(n: int) -> [str]:
 
     result = []
 
-    # Get announcements from the cache (latest first)
-    for id in list(id_cache)[:n]:
-        result.append(post_cache[id])
+    # the res returned are in reverse chronological order
+    # so we need to reverse it first then take the first n
+    res = get_all_announcements()[::-1][:n]
 
-    # Fetch additional announcements if needed
-    if n > CACHE_LEN:
-        posts = get_all_announcements()
-        needed = n - CACHE_LEN
-
-        # The posts returned are in reverse chronological order
-        # So we first need to grab the last CACHE_LEN + 1 posts to avoid duplicates
-        # Then we need to reverse the order and grab the first needed posts
-        for post in posts[-CACHE_LEN - 1:: -1][:needed]:
-            result.append(get_announcement(get_id(post)))
+    for item in res:
+        result.append(get_announcement(get_id(item)))
 
     return result
