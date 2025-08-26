9885b586bd6a1cb8a2cdfd66dd921b77edf1629c
sophie <sophiebsw@gmail.com>
2025-08-25 21:03:31 -0700

    replace user_exists with a call to search.user_exists

--- ocflib/account/validators.py
+++ ocflib/account/validators.py
@@ -6,6 +6,7 @@ import sys
 import cracklib
 
 import ocflib.misc.mail
+import ocflib.account.search as search
 
 RESERVED_USERNAMES = frozenset((
     # Misc
@@ -385,12 +386,7 @@ def validate_password(username, password, strength_check=True):
 # TODO: we have two implementations of this (one here, one in search).
 # one should be removed.
 def user_exists(username):
-    try:
-        pwd.getpwnam(username)
-    except KeyError:
-        return False
-    else:
-        return True
+    return(search.user_exists(username))
 
 
 def username_reserved(username):
